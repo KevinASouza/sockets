e83dd18d31d5189e6a5f266d74c34c6bb2d902bd
Kevin Assunção <kkevin.assuncao@gmail.com>
2026-06-14 10:40:31 -0300

    refatorar: melhorar a função de conexão do servidor TCP e a manipulação de mensagens.

--- Socket_TCP/servidor_tcp.py
+++ Socket_TCP/servidor_tcp.py
@@ -1,26 +1,71 @@
 import socket
 
+def conexao_socket_servidor_tcp(ip: str = 'localhost', porta: int = 9999) -> None:
+    """Cria um socket e disponibiliza-o para conexão.
 
-servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #(tipo_protocolo_ip, tipo_protocolo_transporte)
+    Args:
+        ip (str, optional): Endereço ip do host. . Defaults to 'localhost'.
+        porta (int, optional): Porta na qual o host receberá conexões.. Defaults to 9999.
+    """
+    try:
+        servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #(tipo_protocolo_ip, tipo_protocolo_transporte)
 
-servidor.bind(('localhost', 9999))
+        servidor.bind((ip, porta))
 
-servidor.listen(1)
+        servidor.listen(1)
+        print('Servidor aguardando conexão...')
 
-sock, endereco = servidor.accept()
+        info_socket, endereco_cliente = servidor.accept()
+        print(f'Conexão estabelecida com {endereco_cliente}')
 
-while True:
+        tarefa_socket_servidor_tcp(info_socket)
 
-    mensagem = sock.recv(1024)
+    except socket.error as e:
+        print(f'Erro: {e}\nNão foi possível estabelecer conexão.')
 
-    mensagem_convertida = mensagem.decode()
+    except Exception as ex:
+        print(f'Erro: {ex}\nOcorreu um erro inesperado.')
+        
+    finally:
+        servidor.close()
 
-    if mensagem_convertida == 'sair':
-        break
 
-    print(f'[Colega]: {mensagem_convertida}')
+def tarefa_socket_servidor_tcp(socket_servidor: socket.socket) -> bool:
+    """Tarefa que o socket realiza ao se estabelecer uma conexão com o mesmo.
 
-    sock.send(input('[Você]: ').encode())
- 
-servidor.close()
+    Args:
+        socket_servidor (socket.socket): Recebe o socket criado anteriormente.
 
+    Returns:
+        bool: Retorna um valor genérico True.
+    """
+    try:
+        while True:
+
+            if not mensagem_cliente:
+                break
+            
+            mensagem_cliente = socket_servidor.recv(1024).decode()
+
+            if mensagem_cliente.lower() == 'sair':
+                break
+
+            print(f'[Cliente]: {mensagem_cliente}')
+
+            socket_servidor.send(input('[Você]: ').encode())
+
+    except socket.error as e:
+        print(f'Erro: {e}\nA conexão foi encerrada inesperadamente.')
+
+    except Exception as ex:
+        print(f'Erro: {ex}\nOcorreu um erro inesperado.')
+
+    finally:
+            finalizar_socket(socket_servidor)
+
+    return True
+
+def finalizar_socket(socket_servidor: socket.socket) -> None:
+    socket_servidor.close()
+
+conexao_socket_servidor_tcp()
